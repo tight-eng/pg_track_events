b5f0614957ed7579a1dfea34621617604f0ccd17
Aidan Cunniffe <acunniffe@gmail.com>
2025-05-05 15:02:28 -0400

    fix for wasm import

--- cli/config/wasm.ts
+++ cli/config/wasm.ts
@@ -1,5 +1,6 @@
 import { join } from "path";
 import "../wasm/wasm_exec.js";
+import { embeddedFiles } from "bun";
 
 declare global {
   class Go {
@@ -29,9 +30,25 @@ export async function initWasm() {
 
   const go = new Go(); // Provided by wasm_exec.js
 
-  const wasmBytes = await Bun.file(
-    new URL("../wasm/bin.wasm", import.meta.url)
-  ).arrayBuffer();
+  let wasmBytes: ArrayBuffer;
+  // @todo figure out why this is the only way to get it to load from embedded files.
+  // docs say it should work both ways, as long as we compile the asset into the binary
+  try {
+    const embeddedFile = (embeddedFiles as unknown as { name: string; arrayBuffer(): Promise<ArrayBuffer> }[]).find(
+      i => i.name === "bin.wasm"
+    );
+    if (embeddedFile) {
+      wasmBytes = await embeddedFile.arrayBuffer();
+    } else {
+      throw new Error("Embedded file not found");
+    }
+  } catch (error) {
+    try {
+      wasmBytes = await Bun.file(new URL("../wasm/bin.wasm", import.meta.url)).arrayBuffer();
+    } catch (error) {
+      throw new Error("Failed to load WASM from both embedded files and file system");
+    }
+  }
 
   const result = await WebAssembly.instantiate(wasmBytes, go.importObject);
 
